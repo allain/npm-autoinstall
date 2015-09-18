104a25da747566dc038fd354ae4c9cc05a27c176
Allain Lalonde <allain.lalonde@gmail.com>
2015-09-18 13:50:01 -0400

    properly resolving npm packages from cwd

--- index.js
+++ index.js
@@ -2,30 +2,29 @@ var detective = require('detective');
 var npmi = require('npmi');
 var fs = require('fs');
 var async = require('async');
+var resolve = require('resolve');
 
 module.exports = function (srcPaths, cb) {
   var seen = {};
   var installed = [];
 
-  async.each(srcPaths, function(srcPath, cb) {
+  async.each(srcPaths, function (srcPath, cb) {
     var src = fs.readFileSync(srcPath, 'utf-8');
 
-    var missingDepCount = 0;
+    async.each(detective(src), function (required, cb) {
+      if (required.match(/^[.]/) || seen[required]) return;
 
-    async.each(detective(src), function(required, cb) {
-      try {
-        if (required.match(/^[.]/) || seen[required]) return;
-
-        require(required);
-        cb();
-      } catch (e) {
-        missingDepCount++;
-        installed.push(required);
-        npmi({name: required}, cb);
-      }
-      seen[required] = true;
+      resolve(required, {basedir: process.cwd()}, function(err) {
+        if (err) {
+          installed.push(required);
+          npmi({name: required}, cb);
+        } else {
+          cb(null);
+        }
+        seen[required] = true;
+      });
     }, cb);
-  }, function(err) {
+  }, function (err) {
     if (err) return cb(err);
 
     cb(null, installed);
